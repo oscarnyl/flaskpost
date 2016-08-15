3ffabda882b24ed01c4a5cde1a2dc68144664a84
Oscar Nylander <occz66@gmail.com>
2016-08-15 18:04:02 +0200

    First test with fetching blogposts from database

--- blog.py
+++ blog.py
@@ -1,9 +1,20 @@
+#!/usr/bin/python
+
+import sqlite3
 from flask import Flask
 app = Flask(__name__)
 
+conn = sqlite3.connect("posts.db")
+
 @app.route("/")
 def blog():
-    return "Placeholder!"
+    result = []
+    cursor = conn.cursor()
+    cursor.execute("SELECT post FROM blogposts")
+    for (post,) in cursor.fetchall():
+        result.append(post + "<br>\n")
+
+    return "".join(result)
 
 if __name__ == "__main__":
     app.run()
