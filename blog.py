3f13bb9e5c849bf5a6c2b8f0ca8d93831d5cc3dd
Oscar Nylander <occz66@gmail.com>
2016-08-16 16:30:57 +0200

    Added posting, fixed styles, added some documentation

--- blog.py
+++ blog.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python
 
 import sqlite3
-from flask import Flask, render_template
+import datetime
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 conn = sqlite3.connect("posts.db")
 
+""" Inserts a post into the database, then redirects to the blog front page. """
+@app.route("/api/post", methods=["POST"])
+def api_post():
+    title = request.form["title"]
+    postbody = request.form["postbody"]
+    timestamp = datetime.datetime.now()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO blogposts (title, post, date) VALUES (?, ?, ?)",
+            (title, postbody, timestamp,))
+    conn.commit()
+    return redirect("/")
+
+""" Gets all blog-posts from database, then places them into a template. """
 @app.route("/")
-def blog():
+def blog_main():
     result = []
     cursor = conn.cursor()
     cursor.execute("SELECT title, post, date FROM blogposts")
@@ -15,6 +29,11 @@ def blog():
         result.append((title, post, date))
 
     return render_template("index.html", content=result)
+
+""" Serves a page where blog posts can be inserted into the database. """
+@app.route("/post")
+def blog_post():
+    return render_template("post.html")
 
 if __name__ == "__main__":
     app.run()
