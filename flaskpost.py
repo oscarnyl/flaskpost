3daae24fa803b6e7bf1a26a8554d8303affb833a
Oscar Nylander <occz66@gmail.com>
2016-08-17 20:14:02 +0200

    Refactored all SQL to use SQLAlchemy

--- flaskpost.py
+++ flaskpost.py
@@ -3,66 +3,41 @@
 import sqlite3
 import datetime
 import os
+from flask_sqlalchemy import SQLAlchemy
 from passlib.hash import sha256_crypt
 from flask import Flask, render_template, request, redirect, abort
+
 app = Flask(__name__)
-
-DATABASE_NAME = "blog.db"
-
-needs_setup = False
-conn = None
-blog_title_global = None
+#TODO: Find better home for database file?
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/flaskpost.db"
+db = SQLAlchemy(app)
+db.create_all()
 
 # If the database does not exist, setup needs to be done.
-if not os.path.isfile(DATABASE_NAME):
-    needs_setup = True
-else:
-    conn = sqlite3.connect(DATABASE_NAME)
-    cursor = conn.cursor()
-    cursor.execute("SELECT value FROM metadata WHERE key='blog_title'")
-    (blog_title_global,) = cursor.fetchone()
 
 """ Serves the setup page. This page will in turn call "/api/setup". """
 @app.route("/setup")
 def setup():
-    #TODO: Replace this with a login system
-    if not needs_setup:
-        abort(403) # No setup is actually needed. Abort mission.
     return render_template("setup.html", blog_title="Setup")
 
-""" Saves settings into config file, then sets up database. """
+""" Saves settings to database metadata table, then redirects to main. """
 @app.route("/api/setup", methods=["POST"])
 def api_setup():
-    global needs_setup
-    global conn
     global blog_title_global
+    global needs_setup
 
-    #TODO: Replace this with a login system
-    if not needs_setup:
-        abort(403) # No setup is actually needed. Abort mission.
-
-    blog_title_local = request.form["blog_title"]
+    blog_title = request.form["blog_title"]
     admin_username = request.form["admin_username"]
     admin_password = request.form["admin_password"]
-    password_hash = sha256_crypt.encrypt(admin_password)
 
-    conn = sqlite3.connect(DATABASE_NAME)
-    cursor = conn.cursor()
-    cursor.executescript("""
-    CREATE TABLE blogposts (id INT PRIMARY KEY, title
-    TEXT NOT NULL, post TEXT NOT NULL, date TEXT NOT NULL);
-    CREATE TABLE metadata (key TEXT NOT NULL, value TEXT NOT NULL);
-    CREATE TABLE users (id INT PRIMARY KEY, username TEXT NOT NULL,
-    password TEXT NOT NULL);
-    """)
-    cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)",
-            (admin_username, password_hash,))
-    cursor.execute("INSERT INTO metadata (key, value) VALUES (?, ?)",
-    ("blog_title", blog_title_local,))
-    blog_title_global = blog_title_local
+    db.session.add(Metadata("blog_title", blog_title))
+    db.session.add(Metadata("setup_reverse_canary", "present"))
+    db.session.add(User(admin_username, admin_password))
+    db.session.commit()
 
-    conn.commit()
+    blog_title_global = blog_title
     needs_setup = False
+
     return redirect("/")
 
 """ Inserts a post into the database, then redirects to the blog front page.
@@ -70,32 +45,23 @@ def api_setup():
     timestamp generated inside the function. """
 @app.route("/api/post", methods=["POST"])
 def api_post():
-    #TODO: Disallow access for unauthorized users
-    if needs_setup:
-        return redirect("/setup")
-
+    #TODO: Access only for authorized users
     title = request.form["title"]
     postbody = request.form["postbody"]
-    timestamp = datetime.datetime.now()
 
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO blogposts (title, post, date) VALUES (?, ?, ?)",
-            (title, postbody, timestamp,))
-    conn.commit()
+    db.session.add(Blogpost(title, postbody))
+    db.session.commit()
 
     return redirect("/")
 
-""" Gets all blog-posts from database, then places them into a template. """
+""" Gets all blog-posts from database, then serves them in a page. """
 @app.route("/")
 def blog_main():
     if needs_setup:
         return redirect("/setup")
-
     result = []
-    cursor = conn.cursor()
-    cursor.execute("SELECT title, post, date FROM blogposts")
-    for (title, post, date,) in cursor.fetchall():
-        result.append((title, post, date))
+    for post in Blogpost.query.all():
+        result.append((post.title, post.post, post.date))
 
     return render_template("index.html", content=result,
             blog_title=blog_title_global)
@@ -103,11 +69,46 @@ def blog_main():
 """ Serves a page where blog posts can be inserted into the database. """
 @app.route("/post")
 def blog_post():
-    #TODO: Disallow access for unauthorized users
     if needs_setup:
         return redirect("/setup")
-
+    #TODO: Access only for authorized users
     return render_template("post.html", blog_title=blog_title_global)
 
+class Blogpost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    post = db.Column(db.Text, nullable=False)
+    date = db.Column(db.Text, nullable=False)
+
+    def __init__(self, title, post):
+        self.title = title
+        self.post = post
+        self.date = datetime.datetime.now()
+
+class Metadata(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.Text, nullable=False)
+    value = db.Column(db.Text, nullable=False)
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = sha256_crypt.encrypt(password)
+        #TODO: Set up properties?
+
 if __name__ == "__main__":
+    # If setup_reverse_canary is not present, this evaluates to true.
+    needs_setup = (Metadata.query.filter_by(key="setup_reverse_canary").first() == None)
+    print("DEBUG: needs_setup = {}".format(needs_setup))
+    if not needs_setup:
+        blog_title_global =\
+        Metadata.query.filter_by(key="blog_title").first().value
     app.run()
